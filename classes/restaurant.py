0b5ef7869e3e3e30e816c5d304e764fecb6ef84b
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-06-27 09:17:25 +0300

    Update the restaurant class

--- classes/restaurant.py
+++ classes/restaurant.py
@@ -1,8 +1,10 @@
 class Restaurant:
     """"a simple restaurant class"""
-    def __init__(self, restaurant_name, cuisine_type):
+    def __init__(self, restaurant_name, cuisine_type, number_served = 0):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = number_served
+        
 
     def describe_restaurant(self):
         print(f"{self.cuisine_type} in {self.restaurant_name}")
@@ -10,8 +12,9 @@ class Restaurant:
     def open_restaurant(self):
         print(f"{self.restaurant_name} is open now")
 
-restaurant = Restaurant("Tegem", "Shiro Wot")
+restaurant = Restaurant("Tegem", "Shiro Wot", 13)
 print(restaurant.restaurant_name)
 print(restaurant.cuisine_type)
+print(restaurant.number_served)
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
