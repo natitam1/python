ccb27d2fa0d59fe4408674b4d57be4a8b4286472
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-06-27 09:34:23 +0300

    Add two methods to restaurant class

--- classes/restaurant.py
+++ classes/restaurant.py
@@ -1,9 +1,9 @@
 class Restaurant:
     """"a simple restaurant class"""
-    def __init__(self, restaurant_name, cuisine_type, number_served = 0):
+    def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
-        self.number_served = number_served
+        self.number_served = 0
         
 
     def describe_restaurant(self):
@@ -12,9 +12,20 @@ class Restaurant:
     def open_restaurant(self):
         print(f"{self.restaurant_name} is open now")
 
-restaurant = Restaurant("Tegem", "Shiro Wot", 13)
+    def set_number_served(self, number):
+        self.number_served = number
+
+    def increment_number_served(self, increment):
+        self.number_served += increment
+        
+
+restaurant = Restaurant("Tegem", "Shiro Wot")
 print(restaurant.restaurant_name)
 print(restaurant.cuisine_type)
 print(restaurant.number_served)
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+restaurant.set_number_served(8)
+print(restaurant.number_served)
+restaurant.increment_number_served(1)
+print(restaurant.number_served)
