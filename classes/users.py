81cfa66a2e739128a9a5d3508c2b52d3943a4304
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-06-27 09:56:01 +0300

    Update the users classes with two methods

--- classes/users.py
+++ classes/users.py
@@ -1,11 +1,12 @@
 class User:
-    def __init__(self, first_name, last_name, age, sex, email, city):
+    def __init__(self, first_name, last_name, age, sex, email, city, login_attempts):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.sex = sex
         self.email = email
         self.city = city
+        self.login_attempts = login_attempts
 
     def describe_user(self):
         print("\nBasic Information")
@@ -15,8 +16,29 @@ class User:
     def greet_user(self):
         print(f"Welcome, {self.first_name} {self.last_name}")
 
-user1 = User("Natnael", "Tamiru", 16, "M", "natnael@gmail.com", "Addis Ababa")
-user2 = User("Abebe", "Chala", 22, "M", "abe23@gmail.com", "Hawassa")
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+user1 = User("Natnael", "Tamiru", 16, "M", "natnael@gmail.com", "Addis Ababa" ,1)
+user2 = User("Abebe", "Chala", 22, "M", "abe23@gmail.com", "Hawassa", 2)
 
 print(user2.first_name)
 user2.greet_user()
+print(user2.login_attempts)
+user2.increment_login_attempts()
+print(user2.login_attempts)
+user2.increment_login_attempts()
+print(user2.login_attempts)
+user2.increment_login_attempts()
+print(user2.login_attempts)
+user2.increment_login_attempts()
+print(user2.login_attempts)
+user2.increment_login_attempts()
+print(user2.login_attempts)
+user2.increment_login_attempts()
+print(user2.login_attempts)
+user2.reset_login_attempts()
+print(user2.login_attempts)
