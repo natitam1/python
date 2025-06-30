8397054e9a369b23ea42740f22c2f6fac09355b3
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-06-30 23:11:27 +0300

    Add lottery analysis

--- classes/python_libs/lottery.py
+++ classes/python_libs/lottery.py
@@ -3,5 +3,14 @@ from random import choice
 lottery_list = [1, 2, 3, 4, 5 ,6, 7, 8, 9, 0, 'a', 'b', 'c','d']
 
 print("The winning lottery number is:")
-winner = [choice(list) for _ in range(4)]
+winner = []
+my_ticket = [1, 2, 3, 4]
+num = 0
+while (winner != my_ticket):
+    winner = [choice(lottery_list) for _ in range(4)]
+    num += 1
+
+
 print(winner)
+print("You need this to win:")
+print(num)
