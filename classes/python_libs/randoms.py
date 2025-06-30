1548ca5178c772513a36e8a25d7f91aa6d625977
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-06-30 22:07:32 +0300

    Add standard python libs to the project

---
+++ classes/python_libs/randoms.py
@@ -0,0 +1,8 @@
+from random import randint, choice
+
+random = randint(1, 6)
+print(random)
+
+players = ["Charles", "Martina", "Michael", "Florence", "Eli"]
+first_up = choice(players)
+print(first_up)
