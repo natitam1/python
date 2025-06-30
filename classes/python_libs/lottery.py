b4ccc2bd164acbfb292604fcd38deca90a39607d
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-06-30 23:21:35 +0300

    Finish the lottery project

--- classes/python_libs/lottery.py
+++ classes/python_libs/lottery.py
@@ -1,16 +1,16 @@
 from random import choice
-"""This project demonstrates how to make random lottery"""
-lottery_list = [1, 2, 3, 4, 5 ,6, 7, 8, 9, 0, 'a', 'b', 'c','d']
 
-print("The winning lottery number is:")
-winner = []
+"""This project demonstrates how to simulate winning a lottery"""
+lottery_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c', 'd']
+
 my_ticket = [1, 2, 3, 4]
 num = 0
-while (winner != my_ticket):
+winner = []
+
+while sorted(winner) != sorted(my_ticket):
     winner = [choice(lottery_list) for _ in range(4)]
     num += 1
 
-
-print(winner)
-print("You need this to win:")
-print(num)
+print("🎉 Your ticket won!")
+print("Winning ticket:", winner)
+print(f"It took {num} tries to win.")
