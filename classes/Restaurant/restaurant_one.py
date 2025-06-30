dd2f5e25d5af1d3fa84dae6e67c9bfd105c1251b
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-06-30 21:16:18 +0300

    Update and make restaurant module

---
+++ classes/Restaurant/restaurant_one.py
@@ -0,0 +1,9 @@
+from restaurant import *
+
+instance_1 = IceCreamStand("Tegerm", "Shiro", "Sour")
+instance_2 = IceCreamStand("Tegerm", "Doro", "Bitter")
+instance_3 = IceCreamStand("Tegerm", "Pasta", "Tang")
+
+instance_1.describe_restaurant()
+instance_2.describe_restaurant()
+instance_3.describe_restaurant()
