239ff25f4c3d26cb9d177ec856338e90192cf3e5
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-07 17:17:32 +0300

    Test with pytest fixture decorator

--- Testing Your Code/pytest/test_cities.py
+++ Testing Your Code/pytest/test_cities.py
@@ -5,4 +5,4 @@ def test_city_country():
     assert format_city_name("santiago", "chile" , 5000000) == "Santiago, Chile - Population 5000000"
 
 def test_city_country_population():
-    assert format_city_name("santiago", "chile", population=5000000) == "Santiago, Chile - Population 5000000"
+    assert format_city_name("santiago", "chile", population=5000000) == "Santiago, Chile - Population 5000000" 
