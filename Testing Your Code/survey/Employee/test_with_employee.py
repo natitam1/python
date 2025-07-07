89042d758afbb2c0b3ca07cfab9e63321294826f
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-07 17:50:22 +0300

    Test with dectator fixture

--- Testing Your Code/survey/Employee/test_with_employee.py
+++ Testing Your Code/survey/Employee/test_with_employee.py
@@ -1,13 +1,18 @@
+import pytest
 from employee import Employee
 
-def test_give_default_raise():
+@pytest.fixture
+def New_employee():
+    """Add new employee."""
+    New_employee = Employee("Natnael", "Tamiru", 2000)
+    return New_employee
+
+def test_give_default_raise(New_employee):
     """Test the class with the default value."""
-    new_employee = Employee("Natnael", "Tamiru", 2000)
-    new_employee.give_raise()
-    assert new_employee.annual_salary == 7000
-def test_give_custom_raise():
+    New_employee.give_raise()
+    assert New_employee.annual_salary == 7000
+def test_give_custom_raise(New_employee):
     """Test with custom amount"""
-    new_employee = Employee("Natnael", "Tamiru", 2000)
-    new_employee.give_raise(1000)
-    assert new_employee.annual_salary == 3000
+    New_employee.give_raise(1000)
+    assert New_employee.annual_salary == 3000
     
