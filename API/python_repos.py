814ccd7795c73278579a461c18536434aabdf975
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-20 22:23:45 +0300

    Make an API call and check the response

---
+++ API/python_repos.py
@@ -0,0 +1,9 @@
+import requests
+
+# Make an API call and check the response.
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
--- PART 1/Testing Your Code/survey/survey.py
+++ PART 1/Testing Your Code/survey/survey.py
@@ -1,6 +1,7 @@
 class AnonymousSurvey:
     """Collect anonymous answers to a survey question."""
 
+
     def __init__(self, question):
         """Store a question, and prepare to store responses."""
         self.question = question
